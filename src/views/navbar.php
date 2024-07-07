091f660a103863ff3500d96f64112ecb420fdb0b
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2024-07-08 00:02:12 +0500

    Updated

--- src/views/navbar.php
+++
@@ -1,7 +0,0 @@
-<div class="navbar">
-    <a href="?action=home">Home</a>
-    <a href="?action=login">Login</a>
-    <a href="?action=signup">Sign Up</a>
-    <a href="?action=create_strategy">Create Strategy</a>
-    <a href="?action=view_strategies">My Strategies</a>
-</div>
